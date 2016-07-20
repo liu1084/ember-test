69a959bd6542a4e4eb951f9c7ede3ab1865862c6
Jim Liu <liu1084@163.com>
2016-07-21 01:50:22 +0800

    ember init

---
+++ app/components/people-list-compiled.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  model() {
+    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+  }
+});
+
+//# sourceMappingURL=people-list-compiled.js.map
